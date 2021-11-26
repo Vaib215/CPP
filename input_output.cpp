170991b86de3a39c629565b095ac449c0e73c364
Vaibhav Sisodiya <sisodiya.vaib215@gmail.com>
2021-11-27 01:26:58 +0530

    C++

---
+++ input_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Author: VAIB
+    cout<<"Hello World\n";
+    int num1,num2;
+    cin>>num1>>num2;
+    cout<<num1+num2<<endl;
+return 0;
+}
