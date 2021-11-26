c6ccd2f95a6531b47cfa3082360564cf9977e95f
Vaibhav Sisodiya <sisodiya.vaib215@gmail.com>
2021-11-27 01:43:01 +0530

    C++

---
+++ even_or_odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Author: VAIB
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if (a%2==0)
+    {
+        cout<<a<<" is even\n";
+    }
+    else
+    {
+        cout<<a<<" is odd\n";
+    }
+    
+return 0;
+}
