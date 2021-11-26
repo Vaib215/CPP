8c4b63bcdc64adfb355fd0024e6dddf9a5d6b1cb
Vaibhav Sisodiya <sisodiya.vaib215@gmail.com>
2021-11-27 00:49:17 +0530

    C++

---
+++ datatypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    // Datatypes in CPP
+    int a;
+    cout<<"size of int is "<<sizeof(a)<<" bytes\n";
+    float b;
+    cout<<"size of float is "<<sizeof(b)<<" bytes\n";
+    double c;
+    cout<<"size of double is "<<sizeof(c)<<" bytes\n";
+    char d;
+    cout<<"size of char is "<<sizeof(d)<<" bytes\n";
+    bool e;
+    cout<<"size of bool is "<<sizeof(e)<<" bytes\n";
+    return 0;
+}
+
